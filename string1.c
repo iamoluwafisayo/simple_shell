9c8f5c828b8cdd47d0b8e44f6b9168a52c4bebe5
fmadala@hotmail.com <fmadala@hotmail.com>
2023-04-26 05:54:43 -0700

    string1.c

---
+++ string1.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ * _strcpy - this  copies a string
+ * @dest: a pointer to the copied string
+ * @src: the source of the string
+ *
+ * Return: a pointer to the  destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[x])
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	dest[x] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup - this will duplicate a string
+ * @str: the string to be duplicated
+ *
+ * Return: a pointer a duplicated string
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ * _puts - this prints an input string
+ * @str: a string to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int x = 0;
+
+	if (!str)
+		return;
+	while (str[x] != '\0')
+	{
+		_putchar(str[x]);
+		x++;
+	}
+}
+
+/**
+ * _putchar - writes a character c to stdout
+ * @c: a character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int x;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || x >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, x);
+		x = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[x++] = c;
+	return (1);
+}
